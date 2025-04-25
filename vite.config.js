337492ed4c2f8a8381191ceb4785e6d57ac57b31
Erfanjz7 <erfanbey@gmail.com>
2025-04-25 17:11:39 +0330

    GREAT BUT WRONG DESIGN

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import tailwind from "tailwindcss";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  publicDir: "./static",
+  base: "./",
+  css: {
+    postcss: {
+      plugins: [tailwind()],
+    },
+  },
+});
